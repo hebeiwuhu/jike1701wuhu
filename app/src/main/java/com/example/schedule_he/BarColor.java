d9135a753db8d00c367dca93fb322bceb7b865d1
Losh <1020328058@qq.com>
2021-01-07 21:11:18 +0800

    完善黑夜模式

--- app/src/main/java/com/example/schedule_he/BarColor.java
+++ app/src/main/java/com/example/schedule_he/BarColor.java
@@ -10,18 +10,19 @@ import androidx.core.view.ViewCompat;
 
 public class BarColor {
 
+
     //设置状态栏颜色
-    public static void setStatusBarColor(Activity activity, int statusColor) {
+    public static void setStatusBarColor(Activity activity,   int statusColor) {
         Window window = activity.getWindow();
         //取消状态栏透明
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        //添加Flag把状态栏设为可绘制模式
+        //添加Flag把状态栏设为可�
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         //设置状态栏颜色
         window.setStatusBarColor(statusColor);
-        //设置系统状态栏处于可见状态
+        //设置系统状态栏处于可见�?
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-        //让view不根据系统窗口来调整自己的布局
+        //让view不根据系统窗口来调整自�
         ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
         View mChildView = mContentView.getChildAt(0);
         if (mChildView != null) {
